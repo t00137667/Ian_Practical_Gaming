02fd3827a6cd09067c1f28af3018de7152380dd0
Ian O Regan <Ian.ORegan@students.ittralee.ie>
2019-01-23 13:00:32 +0000

    First Day Commit

--- PracticalGaming/Assets/MovementControlScript.cs
+++ PracticalGaming/Assets/MovementControlScript.cs
@@ -6,11 +6,20 @@ public class MovementControlScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject gameObject = new GameObject()
+		//GameObject gameObject = new GameObject()
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    /// <summary>
+    /// Implement Swaying Based On MouseInput
+    /// </summary>
+    /// <param name="horizontalAxis">Mouse Input For Horizontal Rotation</param>
+    private void Swaying(double horizontalAxis)
+    {
+        throw new System.NotImplementedException();
+    }
 }
