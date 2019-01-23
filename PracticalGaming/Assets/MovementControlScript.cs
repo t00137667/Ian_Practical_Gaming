01f2b562598205f8c23b7ca2f5d449c6fe894e91
Ian O Regan <Ian.ORegan@students.ittralee.ie>
2019-01-23 12:59:43 +0000

    Signed-off-by: Ian O Regan <Ian.ORegan@students.ittralee.ie>

---
+++ PracticalGaming/Assets/MovementControlScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControlScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject gameObject = new GameObject()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
