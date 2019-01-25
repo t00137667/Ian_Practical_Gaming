6234bfa974dd6b85050039df202942c2d7a581fb
Ian O Regan <Ian.ORegan@students.ittralee.ie>
2019-01-25 11:00:37 +0000

    Added method skeletons and value placeholders.
    Signed-off-by: Ian O Regan <Ian.ORegan@students.ittralee.ie>

--- PracticalGaming/Assets/MovementControlScript.cs
+++ PracticalGaming/Assets/MovementControlScript.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class MovementControlScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    Vector3 Vector3 = new Vector3();
+    float turningSpeed;
+    float rotateSpeed;
+    float speedAdjust;
+
+    // Use this for initialization
+    void Start () {
 		//GameObject gameObject = new GameObject()
 	}
 	
@@ -19,6 +24,79 @@ public class MovementControlScript : MonoBehaviour {
     /// </summary>
     /// <param name="horizontalAxis">Mouse Input For Horizontal Rotation</param>
     private void Swaying(double horizontalAxis)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Alter the currentSpeed
+    /// </summary>
+    private void Accelerate(float speedAdjust)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Determines rotation
+    /// </summary>
+    private void Rotate(float turnSpeed)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Rotates the character Clockwise horizontally
+    /// </summary>
+    /// <param name="turnSpeed">Rate of Turn</param>
+    private void RotateClockwise(float turnSpeed)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Rotates the character ClockwiseRotates the character Anti-Clockwise horizontally
+    /// </summary>
+    private void RotateAntiClockwise(float turningSpeed)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Rotates the character Clockwise vertically
+    /// </summary>
+    private void SpinClockwise(float rotateSpeed)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Rotates the character Anti-Clockwise vertically
+    /// </summary>
+    private void SpinAntiClockwise(string rotateSpeed)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Fires the selected weapon
+    /// </summary>
+    private void FireWeapon()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Iterates through the availiable weapons
+    /// </summary>
+    private void SelectWeapon()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Use Special Item
+    /// </summary>
+    private void FireSpecial()
     {
         throw new System.NotImplementedException();
     }
