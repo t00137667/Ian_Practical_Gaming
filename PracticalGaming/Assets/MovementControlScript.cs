0ae4f4085a3fcf461dfdf1fd0fc5ea416efc8bbe
Ian O Regan <Ian.ORegan@students.ittralee.ie>
2019-01-29 13:51:38 +0000

    Added test control method
    Signed-off-by: Ian O Regan <Ian.ORegan@students.ittralee.ie>

--- PracticalGaming/Assets/MovementControlScript.cs
+++ PracticalGaming/Assets/MovementControlScript.cs
@@ -8,16 +8,38 @@ public class MovementControlScript : MonoBehaviour {
     float turningSpeed;
     float rotateSpeed;
     float speedAdjust;
+    float acceleration = 2f;
+
+    //Declare Keys
+
+
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
 
     // Use this for initialization
     void Start () {
-		//GameObject gameObject = new GameObject()
+        Debug.Log("I liiiive!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Mouse X") * rotationSpeed * 0.5f;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, rotation, 0);
+    }
 
     /// <summary>
     /// Implement Swaying Based On MouseInput
