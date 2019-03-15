dda3bbc54a129010a2493c3074c71ecba51196f4
Ian O Regan <Ian.ORegan@students.ittralee.ie>
2019-03-15 11:03:04 +0000

    Projectile Instantiation
    Signed-off-by: Ian O Regan <Ian.ORegan@students.ittralee.ie>

---
+++ PracticalGaming/Assets/FirePointControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePointControl : MonoBehaviour {
+
+
+    public enum ProjectileType { Bullet, Missile, etc}
+
+    public ProjectileType thisIsA;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    internal void YouAreABullet()
+    {
+        thisIsA = ProjectileType.Bullet;
+    }
+}
---
+++ PracticalGaming/Assets/Scripts/SpawnProjectiles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectiles : MonoBehaviour {
+
+    FirePointControl[] firePoints;
+
+    List<FirePointControl> AllBullets;
+    List<FirePointControl> Allmissile;
+    // Use this for initialization
+    void Start () {
+
+        
+        firePoints = gameObject.GetComponentsInChildren<FirePointControl>();
+
+        firePoints[0].YouAreABullet();
+        AllBullets.Add(firePoints[0]);
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+     //   if (Input.GetKey(KeyCode.Space)) firePoints[currentGunIndex].Shoot();
+		
+	}
+}
